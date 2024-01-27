c8b9d2412c64f6b94370c0cce323ae047f8da6bd
aniqhakimm <71704417+aniqhakimm@users.noreply.github.com>
2024-01-27 12:30:47 +0800

    Bullet Movement, lifespan, and Enemy prefab

---
+++ Assets/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 10;
+    public Rigidbody2D rb;
+    public float destroyDelay = 2f; // Set the delay in seconds
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, destroyDelay); // Destroy the bullet after the specified delay
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject); // Destroy the bullet immediately upon hitting something
+    }
+}
